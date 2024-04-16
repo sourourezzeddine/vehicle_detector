0c9b8f46022e67b3f25b8ea13c70c02a6173d880
sourourezzeddine <sourourezzeddine@gmail.com>
2024-04-16 12:00:19 +0100

    made the hardcoded directories configurable from the test_config.json file

--- tests/test_vehicle_detector.py
+++ tests/test_vehicle_detector.py
@@ -1,18 +1,21 @@
 import vehicle_detector
+import json
 
+with open('test_config.json') as f:
+    test_config = json.load(f)
 
-test_image = "/home/pc/vehicle_detector/images/1.png"
-def test_Predict_car_color(test_image):
+test_image = test_config['test_image_color']
+def test_predict_car_color():
     expected_colors = ['Black', 'Blue', 'Brown', 'Green', 'Orange', 'Red', 'Silver', 'White', 'Yellow']
     color = vehicle_detector.predict_car_color(test_image)
     assert  color in expected_colors
 
-im = "/home/pc/vehicle_detector/images/7.png"
-def test_read_license_plate(im):
+im = test_config['test_image_lp']
+def test_read_license_plate():
     text = vehicle_detector.read_license_plate(im)
     assert text == 'B 58 BPS'
 
-def test_predict_generic_nationality(im):
+def test_predict_generic_nationality():
     nationality = vehicle_detector.predict_generic_nationality (im)
     assert nationality == 'europe'
 
