3e06dd699359fdf9c6d256f2feacfebfe17e24f6
sourourezzeddine <sourourezzeddine@gmail.com>
2024-04-18 16:08:39 +0100

    added the error handling on file opening

--- tests/test_vehicle_detector.py
+++ tests/test_vehicle_detector.py
@@ -1,21 +1,25 @@
-import vehicle_detector
+import main
 import json
 
-with open('test_config.json') as f:
-    test_config = json.load(f)
+
+try:
+   with open('test_config.json') as f:
+        test_config = json.load(f)
+except :
+    raise FileNotFoundError("The file 'MQTT_config.json' was not found.")
 
 test_image = test_config['test_image_color']
 def test_predict_car_color():
     expected_colors = ['Black', 'Blue', 'Brown', 'Green', 'Orange', 'Red', 'Silver', 'White', 'Yellow']
-    color = vehicle_detector.predict_car_color(test_image)
+    color = main.predict_car_color(test_image)
     assert  color in expected_colors
 
 im = test_config['test_image_lp']
 def test_read_license_plate():
-    text = vehicle_detector.read_license_plate(im)
+    text = main.read_license_plate(im)
     assert text == 'B 58 BPS'
 
 def test_predict_generic_nationality():
-    nationality = vehicle_detector.predict_generic_nationality (im)
+    nationality = main.predict_generic_nationality (im)
     assert nationality == 'europe'
 
